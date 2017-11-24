24165d27576336d9720aab07621737ab59ff33bd
vinnyA3 <vincent.aceto@gmail.com>
2017-11-24 14:16:25 -0500

    feat(install): create new sym links

--- install.sh
+++ install.sh
@@ -5,6 +5,13 @@ DOTFILES_DIR=cd "$(dirname "${BASH_SOURCE[0]}")" && pwd
 # Create symlinks
 
 ln -sfv $DOTFILES_DIR/runcom/.bash_profile ~
-ln -sfv $DOTFILES_DIR/runcom/.git/.gitconfig ~
-ln -sfv $DOTFILES_DIR/runcom/vim ~/.vim
-
+ln -sfv $DOTFILES_DIR/git/.gitconfig ~
+ln -sfv $DOTFILES_DIR/config ~/.config
+ln -sfv $DOTFILES_DIR/colors ~/.colors
+ln -sfv $DOTFILES_DIR/vim ~/.vim
+ln -sfv $DOTFILES_DIR/nvm ~/.nvm
+ln -sfv $DOTFILES_DIR/tmux/.tmux.conf ~/.tmux.conf
+ln -sfv $DOTFILES_DIR/xmonad ~/.xmonad
+ln -sfv $DOTFILES_DIR/x-files/.xinitrc ~/.xinitrc
+ln -sfv $DOTFILES_DIR/x-files/.xserverrc ~/.xserverrc
+ln -sfv $DOTFILES_DIR/xfiles/.Xresources ~/.Xresources
