a962ad8b0a78e72440aea4ae20d8f2b3004802ad
vinnyA3 <vincent.aceto@gmail.com>
2017-12-27 20:55:09 -0500

    feat(xmonad): new functionality
    Changed default terminal to alacritty. Moved dynamic xmobar colors to
    variables. Configured some apps to open in appropriate workspaces on
    startup.
    Removed border for single / fullscreen windows. Add dmenu args
    (x,y,colors ...)

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,35 +1,90 @@
 import XMonad
 import XMonad.Config.Desktop
-import XMonad.Layout.BinarySpacePartition
+import XMonad.Layout.NoBorders
 import XMonad.Layout.Spacing
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.Run
 import XMonad.Util.EZConfig
 
+-- DefaultTerminal: Set to Termite
+myTerminal     = "alacritty"
+
+-- Launcher: Set to Dmenu
+myLauncher     = "dmenu_run -p '\xf00e' -b -x 380 -y 9 -W 620 -i -fn 'Noto Sans UI-10' -nb '#282A36' -nf '#F8F8F2' -sb '#50FA7B' -sf '#282A36'"
+
 -- ModKey: Set to Windows Key
-modm = mod4Mask
--- TODO: Get these colors from xrdb
-backgroundColor   = "#FEFEFE"
-middleColor       = "#AEAEAE"
-foregroundColor   = "#0E0E0E"
+modm           = mod4Mask
+
+-- Border Styling
+myBorderWidth  = 1
+
+myNormalBorderColor = "#BFBFBF"
+
+myFocusedBorderColor = "#FF79C6"
+
+-- Xmobar dyn colors
+xmobarCurrFG  = "#282A36"
+
+xmobarCurrBG = "#CAA9FA"
+
+xmobarHiddenFG = "#747C84"
+
+-- My Workspaces
+myWorkspaces   = [ "1:  \xf120  "
+                 , "2:  \xf268  "
+                 , "3:  \xf121  "
+                 , "4:  \xf1bc  "
+                 , "5:  \xf269  "
+                 , "6:  \xf259  "
+                 ] ++ map show [7..9]
+
+-- My Layout Hook
+myLayout       = avoidStruts $ smartBorders $ smartSpacingWithEdge 8  $  Tall 1 (3/100) (1/2)
+
+-- myManageHook
+myManageHook   = composeAll
+    [ className =? "Chromium" --> doShift "2:  \xf268  "
+    , className =? "spotify"  --> doShift "4:  \xf1bc  "
+    , className =? "Firefox"  --> doShift "5:  \xf269  "
+    , manageDocks
+    ]
+
+-- myNewManageHook   = manageHook desktopConfig <+> manageDocks
+myNewManageHook   = myManageHook <+> manageHook desktopConfig
+
+main :: IO ()
 
 main = do
-    xmonad $ desktopConfig
-        { borderWidth        = 1
-        , focusedBorderColor = foregroundColor
-        , focusFollowsMouse  = False
-        , layoutHook         = avoidStruts $ spacingWithEdge 8 emptyBSP
-        , modMask            = modm 
-        , normalBorderColor  = middleColor
-        , terminal           = "termite"
-        , workspaces         = [ "browse", "code", "read", "chat", "etc" ]
-        } `additionalKeys`
-        [ ((modm, xK_p), spawn "rofi -show run")
-        ] `additionalKeysP`
-        [ ("<XF86AudioRaiseVolume>", spawn "pactl set-sink-mute 0 false ; pactl set-sink-volume 0 +5%")
-        , ("<XF86AudioLowerVolume>", spawn "pactl set-sink-mute 0 false ; pactl set-sink-volume 0 -5%")
-        , ("<XF86AudioMute>", spawn "pactl set-sink-mute 0 toggle")
-        , ("<XF86AudioNext>", spawn "playerctl next")
-        , ("<XF86AudioPrev>", spawn "playerctl previous")
-        , ("<XF86AudioPlay>", spawn "playerctl play-pause")
-        ]
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ defaults {
+        logHook      = dynamicLogWithPP $ xmobarPP {
+            ppOutput    = hPutStrLn xmproc
+            , ppCurrent = xmobarColor xmobarCurrFG xmobarCurrBG . wrap "   " "   "
+            , ppHidden  = xmobarColor xmobarHiddenFG "" . wrap "   " "   "
+            , ppTitle   = (\str -> "")
+            , ppLayout  = (\str -> "")
+        }
+        , manageHook = myNewManageHook
+    }
+
+defaults = desktopConfig
+    { borderWidth = myBorderWidth 
+    , normalBorderColor = myNormalBorderColor
+    , focusedBorderColor = myFocusedBorderColor
+    , modMask     = modm 
+    , terminal    = myTerminal 
+    , workspaces  = myWorkspaces
+    , layoutHook  = myLayout
+    } `additionalKeys`
+    [ ((modm, xK_p), spawn myLauncher)
+    -- , ((modm .|. controlMask, xK_g), sendMessage (ModifySpacing 0))  -- toggle all gaps
+    ] `additionalKeysP`
+    [ ("<XF86AudioRaiseVolume>", spawn "pactl set-sink-mute @DEFAULT_SINK@ false ; pactl set-sink-volume @DEFAULT_SINK@ +5%")
+    , ("<XF86AudioLowerVolume>", spawn "pactl set-sink-mute @DEFAULT_SINK@ false ; pactl set-sink-volume @DEFAULT_SINK@ -5%")
+    , ("<XF86AudioMute>", spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
+    , ("<XF86AudioNext>", spawn "playerctl next")
+    , ("<XF86AudioPrev>", spawn "playerctl previous")
+    , ("<XF86AudioPlay>", spawn "playerctl play-pause")
+    ]
 
