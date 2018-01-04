6a96c3785680728c2c68ea817e9de9af8d36500a
vinnyA3 <vincent.aceto@gmail.com>
2018-01-04 14:26:09 -0500

    fix(install): make utils available

--- install.sh
+++ install.sh
@@ -1,13 +1,23 @@
 # Get the current dir - now we can run this script from anywhere
 
+export DOTFILES_DIR
+
 DOTFILES_DIR=cd "$(dirname "${BASH_SOURCE[0]}")" && pwd
+
+# Make utilities available
+
+PATH="$DOTFILES_DIR/bin:$PATH"
+
+# Update dotfiles itself first
+
+if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; fi
 
 # Create symlinks
 
 ln -sfv $DOTFILES_DIR/runcom/.bash_profile ~
 ln -sfv $DOTFILES_DIR/git/.gitconfig ~
 ln -sfv $DOTFILES_DIR/config ~/.config
-ln -sfv $DOTFILES_DIR/colors ~/.colors
+# ln -sfv $DOTFILES_DIR/colors ~/.colors
 ln -sfv $DOTFILES_DIR/vim ~/.vim
 ln -sfv $DOTFILES_DIR/nvm ~/.nvm
 ln -sfv $DOTFILES_DIR/tmux/.tmux.conf ~/.tmux.conf
