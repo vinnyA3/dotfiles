ab341ea299fafccc2f834fff9696610d46090b92
vinnyA3 <vincent.aceto@gmail.com>
2018-02-24 23:53:49 -0500

    fix(install): sym link filename error

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ ln -sfv $DOTFILES_DIR/config ~/.config
 # ln -sfv $DOTFILES_DIR/colors ~/.colors
 ln -sfv $DOTFILES_DIR/vim ~/.vim
 ln -sfv $DOTFILES_DIR/nvm ~/.nvm
-ln -sfv $DOTFILES_DIR/tmux/.tmux.conf ~/.tmux.conf
+ln -sfv $DOTFILES_DIR/tmux/tmux.conf ~/.tmux.conf
 ln -sfv $DOTFILES_DIR/xmonad ~/.xmonad
 ln -sfv $DOTFILES_DIR/x-files/.xinitrc ~/.xinitrc
 ln -sfv $DOTFILES_DIR/x-files/.xserverrc ~/.xserverrc
