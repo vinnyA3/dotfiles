c1c72dbe0b255a6e60fdbee76b3df664c562fd2c
vinnyA3 <vincent.aceto@gmail.com>
2018-05-20 13:43:45 -0400

    feat(install): add stow commands / remove manual sym links

--- install.sh
+++ install.sh
@@ -13,16 +13,14 @@ if [is-executable git -a -d "$DOTFILES_DIR/.git"]; then
     git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" submodule init
 fi
 
-# Create symlinks
-ln -sfv $DOTFILES_DIR/runcom/.bash_profile ~
-ln -sfv $DOTFILES_DIR/git/.gitconfig ~
-ln -sfv $DOTFILES_DIR/config ~/.config
-# ln -sfv $DOTFILES_DIR/colors ~/.colors
-ln -sfv $DOTFILES_DIR/vim ~/.vim
+# Create symlinks (nvm) refactor to use stow ... later
 ln -sfv $DOTFILES_DIR/nvm ~/.nvm
-ln -sfv $DOTFILES_DIR/tmux/tmux.conf ~/.tmux.conf
-ln -sfv $DOTFILES_DIR/xmonad ~/.xmonad
-ln -sfv $DOTFILES_DIR/x-files/.xinitrc ~/.xinitrc
-ln -sfv $DOTFILES_DIR/x-files/.xserverrc ~/.xserverrc
-ln -sfv $DOTFILES_DIR/xfiles/.Xresources ~/.Xresources
 
+# Stow
+stow runcom
+stow git
+stow config
+stow vim
+stow tmux
+stow xmonad
+stow x-files
