6466324e6f91999b57533199e3711a6caadadc9a
vinnyA3 <vincent.aceto@gmail.com>
2018-05-25 15:15:02 -0400

    feat(xmonad): add new functionality + formatting
    Added new functionality ... notably, added floating window
    accessibility (control next win float) and added new spiral
    layout algorithm.  In addition, changed the resizable tall key-
    bindings (they were originally unintuitive).
    
    Finally, I just some style changes (formatting ..etc) to maitain
    config consistency.

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -2,74 +2,74 @@ import XMonad
 import XMonad.Config.Desktop
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Spacing
+import XMonad.Layout.Spiral
 import XMonad.Layout.ResizableTile
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.FloatNext
 import XMonad.Util.Run
 import XMonad.Util.Loggers
 import XMonad.Util.EZConfig
 import Data.Char
 
--- DefaultTerminal: Set to Termite
-myTerminal     = "alacritty"
+-- DefaultTerminal: Set to St (Alacritty, Termite)
+myTerminal = "st"
 
--- Launcher: Set to Dmenu
-myLauncher     = "dmenu_run -p '\xf00e' -x 380 -y 9 -W 620 -i -fn 'Noto Sans UI-10' -nb '#282A36' -nf '#F8F8F2' -sb '#50FA7B' -sf '#282A36'"
+-- Launcher: Set to Dmenu (x: 380)
+myLauncher = "dmenu_run -b -p '\xf00e' -x 10 -y 10 -W 620 -i -fn 'Noto Sans UI-10' -nb '#282A36' -nf '#F8F8F2' -sb '#50FA7B' -sf '#282A36'"
 
 -- ModKey: Set to Windows Key
-modm           = mod4Mask
+modm = mod4Mask
 
 -- Border Styling
-myBorderWidth  = 1
+myBorderWidth = 1
 
 myNormalBorderColor = "#BFBFBF"
 
 myFocusedBorderColor = "#FF79C6"
 
 -- Xmobar dyn colors
-xmobarCurrFG  = "#282A36"
+xmobarCurrFG = "#282A36"
 
 xmobarCurrBG = "#FF79C6"
 
 xmobarHiddenFG = "#747C84"
 
 -- My Workspaces
-myWorkspaces = [ "Terminal"
-                  ,"Qutebrowser"
-                  ,"Code"
-                  ,"Firefox"
-                  ,"Media"
-                  ,"Misc"
-                  ,"Alt7"
-                  ,"Alt8"
-                  ,"Alt9" ]
+myWorkspaces = [ " Terminal"
+                  ," Qutebrowser"
+                  ," Code"
+                  ," Firefox"
+                  ," Chrome"
+                  ," Media"
+                  ," Alt7"
+                  ," Alt8"
+                  ," Alt9" ]
 
 --myWorkspaces   =  map show $ take 9 [1..] 
 
 -- My Layout Hook
-myLayout = avoidStruts $ smartBorders $ smartSpacingWithEdge 8 $ ResizableTall 1 (3/100) (1/2) []
+myLayout = avoidStruts $ smartBorders $ smartSpacingWithEdge 8 $ (ResizableTall 1 (3/100) (1/2) [] ||| spiral (6/7))
 
 -- myManageHook
 myManageHook = composeAll
-    [ className =? "Chromium" --> doShift "2  \xf268  "
-    , className =? "Spotify"  --> doShift "4  \xf1bc  "
-    , className =? "Firefox"  --> doShift "5  \xf269  "
+    [ className =? "qutebrowser" --> doShift " Qutebrowser"
+    , className =? "Spotify"  --> doShift " Media"
+    , className =? "Firefox"  --> doShift " Firefox"
+    , className =? "Chromium"  --> doShift " Chrome"
     , manageDocks ]
 
 -- myNewManageHook   = manageHook desktopConfig <+> manageDocks
-myNewManageHook   = myManageHook <+> manageHook desktopConfig
-
-tagL l = onLogger $ wrap(l ++ " :: ") ""
+myNewManageHook   = myManageHook <+> floatNextHook <+> manageHook desktopConfig
 
 main :: IO ()
-
 main = do
     xmproc <- spawnPipe "xmobar > /dev/null 2>&1"
     xmonad $ defaults {
         logHook      = dynamicLogWithPP $ xmobarPP {
             ppOutput    = hPutStrLn xmproc
-            , ppCurrent = xmobarColor "" xmobarCurrBG . shorten 4 
-            , ppHidden  = xmobarColor "" xmobarHiddenFG . shorten 4 
+            , ppCurrent = xmobarColor "" xmobarCurrBG . shorten 5 
+            , ppHidden  = xmobarColor "" xmobarHiddenFG . shorten 5
             , ppSep = "   " 
             , ppWsSep = "   "
             , ppExtras = [fmap (\m -> fmap (\s -> "\xf178  " ++ s) m) $ logCurrent]
@@ -89,8 +89,10 @@ defaults = desktopConfig
     , layoutHook  = myLayout
     } `additionalKeys`
     [ ((modm, xK_p), spawn myLauncher)
-    , ((modm, xK_a), sendMessage MirrorShrink)
-    , ((modm, xK_z), sendMessage MirrorExpand)
+    , ((modm, xK_z), sendMessage MirrorShrink)
+    , ((modm, xK_a), sendMessage MirrorExpand) 
+    , ((modm, xK_e), toggleFloatNext)
+    -- easier keybindings for media keys
     ] `additionalKeysP`
     [ ("<XF86AudioRaiseVolume>", spawn "pactl set-sink-mute @DEFAULT_SINK@ false ; pactl set-sink-volume @DEFAULT_SINK@ +5%")
     , ("<XF86AudioLowerVolume>", spawn "pactl set-sink-mute @DEFAULT_SINK@ false ; pactl set-sink-volume @DEFAULT_SINK@ -5%")
