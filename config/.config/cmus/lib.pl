506a750d2a05525f1ae268e5d028b165456a1635
vinnyA3 <vincent.aceto@gmail.com>
2018-05-26 04:51:27 -0400

    feat(config): add configs

---
+++ config/.config/cmus/lib.pl
@@ -0,0 +1,77 @@
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - Something Good By alt-J.m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - In My Mind By Amanda Palmer Feat  Brian Viglione.m4a
+/home/qwerty/Music/mps/Andrew Luce & Alexander Lewis - Take Me (feat. Jojee).m4a
+/home/qwerty/Music/mps/Angus & Julia Stone - I'm Not Yours (Fytch Remix).mp3
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - Santa Monica Dream By Angus & Julia Stone.m4a
+/home/qwerty/Music/mps/Arty - Falling Down (feat. Maty Noyes).m4a
+/home/qwerty/Music/mps/Attack on Titan - 'Call Your Name' with Lyrics.m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - Got Well Soon By Breton.m4a
+/home/qwerty/Music/mps/Chelsea Cutler - The Reason.m4a
+/home/qwerty/Music/mps/Felix Cartal - Worry (feat. Victoria Zaro).m4a
+/home/qwerty/Music/mps/Flume - Say It (feat. Tove Lo) (Illenium Remix).m4a
+/home/qwerty/Music/mps/Flume - Never Be Like You feat. Kai.m4a
+/home/qwerty/Music/mps/Flume - Take a Chance feat. Little Dragon.m4a
+/home/qwerty/Music/mps/Flume - Wall Fuck.m4a
+/home/qwerty/Music/mps/Flume - Hyperreal feat. Kučka.m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - Spanish Sahara By Foals.m4a
+/home/qwerty/Music/mps/HALIENE - Dream In Color.m4a
+/home/qwerty/Music/mps/[Progressive House] - Hellberg - The Girl (feat. Cozi Zuehlsdorff) [Monstercat Release].m4a
+/home/qwerty/Music/mps/Izzard & Blankts - Threads.m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - Blackwell Academy By Jonathan Morali.m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - Golden hour By Johnathan Morali.m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - Kate By Jonathan Morali.m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack Max & Chloe By Jonathan Morali.m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - Night Walk By Jonathan Morali.m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - The Storm By Jonathan Morali.m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - Timeless By Jonathan Morali.m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - Timelines By Jonathan Morali.m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - Crosses By José González.m4a
+/home/qwerty/Music/mps/Beneath with Me (feat. Skylar Grey) (Kaskade's V.4).m4a
+/home/qwerty/Music/mps/Khamsin - My Way (feat. MOONZz).m4a
+/home/qwerty/Music/mps/Kill Paris - Made of Time (feat. Trove & Emily Vaughn).m4a
+/home/qwerty/Music/mps/Heavy (feat. Kiiara) (Disero Remix).m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - Mt. Washington By Local Natives.m4a
+/home/qwerty/Music/mps/London Grammar - Hey Now [Arty remix].m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - Lua By Bright Eyes.m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - Mountains By Message To Bears.m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - Kids Will Be Skeletons By Mogwai.m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - The Sense Of Me By Mudflow.m4a
+/home/qwerty/Music/mps/Bury Me In Black (Demo).m4a
+/home/qwerty/Music/mps/Cemetery Drive.m4a
+/home/qwerty/Music/mps/Desert Song.m4a
+/home/qwerty/Music/mps/Give 'Em Hell, Kid.m4a
+/home/qwerty/Music/mps/Hang 'Em High.m4a
+/home/qwerty/Music/mps/Helena (So Long & Goodnight).m4a
+/home/qwerty/Music/mps/I'm Not Okay (I Promise).m4a
+/home/qwerty/Music/mps/I Never Told You What I Do For A Living.m4a
+/home/qwerty/Music/mps/Interlude.m4a
+/home/qwerty/Music/mps/It's Not A Fashion Statement It's A Deathwish.m4a
+/home/qwerty/Music/mps/Thank You For The Venom.m4a
+/home/qwerty/Music/mps/The Ghost Of You.m4a
+/home/qwerty/Music/mps/The Jetset Life Is Gonna Kill You.m4a
+/home/qwerty/Music/mps/To The End.m4a
+/home/qwerty/Music/mps/You Know What They Do To Guys Like Us In Prison.m4a
+/home/qwerty/Music/mps/Blood (Hidden Track).m4a
+/home/qwerty/Music/mps/Cancer.m4a
+/home/qwerty/Music/mps/Dead!.m4a
+/home/qwerty/Music/mps/Disenchanted.m4a
+/home/qwerty/Music/mps/Famous Last Words.m4a
+/home/qwerty/Music/mps/House of Wolves.m4a
+/home/qwerty/Music/mps/I Don't Love You.m4a
+/home/qwerty/Music/mps/Mama.m4a
+/home/qwerty/Music/mps/Sleep.m4a
+/home/qwerty/Music/mps/Teenagers.m4a
+/home/qwerty/Music/mps/The End..m4a
+/home/qwerty/Music/mps/The Sharpest Lives.m4a
+/home/qwerty/Music/mps/This Is How I Disappear.m4a
+/home/qwerty/Music/mps/Welcome to the Black Parade.m4a
+/home/qwerty/Music/mps/Said The Sky - Over Getting Over You (feat. Matthew Koma).m4a
+/home/qwerty/Music/mps/Seven Lions Feat. Runn - Calling You Home.m4a
+/home/qwerty/Music/mps/Seven Lions, Tritonal & Kill The Noise Feat. HALIENE - Horizon.m4a
+/home/qwerty/Music/mps/Shaun Frank & Krewella - Gold Wings.m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - Piano Fire By Sparklehorse.m4a
+/home/qwerty/Music/mps/Sweater Beats x Alyson Stoner x BKAYE - Symphony.m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - Obstacles By Syd Matters.m4a
+/home/qwerty/Music/mps/Life Is Strange Soundtrack - To All Of You By Syd Matters.m4a
+/home/qwerty/Music/mps/Wonders [AGST Remix] by Ten Towers - [Beats Music].m4a
+/home/qwerty/Music/mps/Tom Day & Monsoonsiren - Leviathan.m4a
