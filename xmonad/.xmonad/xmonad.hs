a8a674cb0824876d20a7bf0e12d7516b45c4c7c7
vinnyA3 <vincent.aceto@gmail.com>
2018-06-30 22:41:35 -0400

    feat(xmonad): dmenu config update

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -18,7 +18,7 @@ import Data.Char
 myTerminal = "st"
 
 -- Launcher: Set to Dmenu (x: 380)
-myLauncher = "dmenu_run -b -p '\xf00e' -x 8 -y 10 -W 620 -i -fn 'Noto Sans UI-10' -nb '#282A36' -nf '#F8F8F2' -sb '#50FA7B' -sf '#282A36'"
+myLauncher = "dmenu_run -l 5 -p '\xf303' -x 8 -y 9 -w 620 -i -fn 'FuraCode Nerd Font-10' -nb '#282A36' -nf '#F8F8F2' -sb '#50FA7B' -sf '#282A36'"
 
 -- ModKey: Set to Windows Key
 modm = mod4Mask
@@ -75,7 +75,7 @@ main = do
             , ppCurrent = xmobarColor "" xmobarCurrBG . shorten 1
             , ppHidden  = xmobarColor "" xmobarHiddenFG . shorten 1
             , ppSep = " "  
-            , ppWsSep = "  "
+            , ppWsSep = " "
             , ppExtras = [fmap (\m -> fmap (\s -> "\xfc96" ++ s) m) $ logCurrent]
             , ppTitle   = (\str -> "")
             , ppLayout  = (\str -> "")
