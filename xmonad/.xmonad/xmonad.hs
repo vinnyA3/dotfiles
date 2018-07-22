29f2bebec1e24821c589a2609d9b9d1e0b82268e
vinnya3 <vincent.aceto@gmail.com>
2018-07-22 01:10:20 -0400

    feat(xmonad): add scratchpad fns, formatting

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -10,6 +10,7 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.FloatNext
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
+import XMonad.Util.NamedScratchpad
 import XMonad.Layout
 import XMonad.Layout.Accordion
 import XMonad.Layout.NoBorders
@@ -29,6 +30,7 @@ import qualified Data.Map as M
 
 -- DefaultTerminal: Set to succless terminal (Alacritty, Termite)
 myTerminal = "st -e '/run/current-system/sw/bin/elvish'"
+-- myTerminal = "xterm"
 
 -- Launcher: Set to Rofi (x: 380)
 myLauncher = "rofi -show drun"
@@ -96,8 +98,21 @@ myManageHook = composeAll
   , className =? "Chromium" --> doShift " Chrome"
   ]
 
-myNewManageHook =
-  composeAll [myManageHook, floatNextHook, manageHook desktopConfig]
+scratchpads =
+  [ NS "htop" "st -t process -e htop" (title =? "process") defaultFloating
+  , NS "cmus"
+       "st -t cmus -e cmus"
+       (title =? "cmus")
+       (customFloating $ W.RationalRect (1 / 6) (1 / 6) (2 / 3) (2 / 3))
+  ]
+
+myNewManageHook = composeAll
+  [ myManageHook
+  , floatNextHook
+  , manageHook desktopConfig
+  , namedScratchpadManageHook scratchpads
+  ]
+
 
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf@(XConfig { XMonad.modMask = modMask }) =
@@ -106,6 +121,13 @@ myKeys conf@(XConfig { XMonad.modMask = modMask }) =
        , ((modm, xK_p)                   , spawn myLauncher)
        , ((modm, xK_Tab)                 , nextWS)
        , ((modm .|. shiftMask, xK_Tab)   , prevWS)
+       , ( (modm .|. controlMask .|. shiftMask, xK_t)
+         , namedScratchpadAction scratchpads "htop"
+         )
+       , ( (modm .|. controlMask .|. shiftMask, xK_c)
+         , namedScratchpadAction scratchpads "cmus"
+         )
+       -- audio keybindings
        , ( (0, xF86XK_AudioRaiseVolume)
          , spawn
            "pactl set-sink-mute @DEFAULT_SINK@ false ; pactl set-sink-volume @DEFAULT_SINK@ +5%"
@@ -176,7 +198,9 @@ myKeys conf@(XConfig { XMonad.modMask = modMask }) =
        , ( (mod4Mask, xK_q)
          , broadcastMessage ReleaseResources >> restart "xmonad" True
          ) -- %! Restart xmonad
-       , ((modm .|. shiftMask, xK_x), spawn "kill $(pidof xmobar); xmobar ~/.config/xmobar/xmobarrc.hs") -- %! Kill & restart xmobar
+       , ( (modm .|. shiftMask, xK_x)
+         , spawn "kill $(pidof xmobar); xmobar ~/.config/xmobar/xmobarrc.hs"
+         ) -- %! Kill & restart xmobar
        , ((modm, xK_z)              , sendMessage MirrorShrink)
        , ((modm, xK_a)              , sendMessage MirrorExpand)
        , ((modm, xK_e)              , toggleFloatNext)
@@ -186,9 +210,7 @@ myKeys conf@(XConfig { XMonad.modMask = modMask }) =
          )
                         -- toggle fullscreen (really just lower status bar
                         --    below everything)
-       , ( (modm .|. shiftMask, xK_f)
-         , sendMessage ToggleStruts
-         )
+       , ((modm .|. shiftMask, xK_f), sendMessage ToggleStruts)
                       -- floating window keys
        -- , ((modm .|. shiftMask, xK_Up), withFocused (keysMoveWindow (0, -10)))
        -- , ((modm .|. shiftMask, xK_Down), withFocused (keysMoveWindow (0, 10)))
@@ -230,7 +252,7 @@ myXmobarPP = def { ppCurrent = xmobarColor xmobarCurrBG "" . splitTakeFst
 
 main :: IO ()
 main = do
-  spawn xmproc 
+  spawn xmproc
   xmonad $ defaults { logHook = dynamicLogString myXmobarPP >>= xmonadPropLog }
 
 defaults = docks $ desktopConfig
