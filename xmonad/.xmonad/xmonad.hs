ea758cf22687749435515e7f3db42709abe09f0a
vinnyA3 <vincent.aceto@gmail.com>
2018-08-02 11:34:53 -0400

    fix(xmonad): syntax + change initial wallpaper

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -100,7 +100,7 @@ myManageHook = composeAll
 
 scratchpads =
   [ NS "htop" "st -t process -e htop" (title =? "process")  defaultFloating
-  , NS "chat" "st -t chat -e weechat"    (title =? "chat") defaultFloating
+  , NS "chat" "st -t chat -e weechat" (title =? "chat")     defaultFloating
   , NS "cmus" "st -c cmus -e cmus"    (className =? "cmus") defaultFloating
   ]
 
@@ -125,7 +125,9 @@ myKeys conf@(XConfig { XMonad.modMask = modMask }) =
        , ( (modm .|. controlMask .|. shiftMask, xK_c)
          , namedScratchpadAction scratchpads "cmus"
          )
-       , ((modm .|. controlMask .|. shiftMask, xK_w), namedScratchpadAction scratchpads "chat"
+       , ( (modm .|. controlMask .|. shiftMask, xK_w)
+         , namedScratchpadAction scratchpads "chat"
+         )
        -- audio keybindings
        , ( (0, xF86XK_AudioRaiseVolume)
          , spawn
@@ -265,6 +267,6 @@ defaults = docks $ desktopConfig
   , manageHook         = myNewManageHook
   , layoutHook = avoidStruts $ smartBorders $ smartSpacingWithEdge 8 $ myLayout
   , startupHook        = spawn
-    "feh --bg-scale /home/qwerty/Pictures/wallpaper/geometry-dracula.png"
+    "feh --bg-scale /home/qwerty/Pictures/wallpaper/ururaka.png"
   }
 
