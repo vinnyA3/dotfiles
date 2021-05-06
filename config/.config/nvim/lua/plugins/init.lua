815adc8dc02e72299a5b0c773dd9106e15333844
vinnyA3 <vincent.aceto@gmail.com>
2021-05-06 16:12:27 -0400

    feat(lua): add vim-unimpaired plug

--- config/.config/nvim/lua/plugins/init.lua
+++ config/.config/nvim/lua/plugins/init.lua
@@ -27,6 +27,7 @@ return require('packer').startup(
     use 'tpope/vim-commentary'
     use 'tpope/vim-surround'
     use 'tpope/vim-repeat'
+    use 'tpope/vim-unimpaired'
     use 'mattn/emmet-vim'
     use 'jiangmiao/auto-pairs' -- TODO: find lua alternative
 
